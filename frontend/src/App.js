8aff5e3249a4ac945d7ebbd1f2f4af83260dcc68
SVJLucas <lucas.jose.veloso.de.souza@gmail.com>
2024-03-23 13:47:56 +0100

    conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,7 @@ import DecisionPage from './pages/Decision/Decision.js';
 import LogIn from './pages/Login/LogIn'
 import SignUp from './pages/Login/SignUp.js';
 import CreateESOP from './pages/Company/CreateESOP/CreateESOP.js';
+import Dashboard from './pages/Company/Dashboard/Dashboard.js';
 
 /* FIXME: Step 3.1 */
 
@@ -110,6 +111,7 @@ function App() {
         <Route path="/decision" element={<DecisionPage />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/createesop" element={<CreateESOP />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
