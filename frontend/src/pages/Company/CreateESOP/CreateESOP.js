694043eee40d293c5c4dddd60f898a9385491a08
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2024-03-24 03:26:53 +0100

    fixing esop page

--- frontend/src/pages/Company/CreateESOP/CreateESOP.js
+++ frontend/src/pages/Company/CreateESOP/CreateESOP.js
@@ -190,11 +190,10 @@ export default function CreateESOP() {
             {activeStep === steps.length ? (
               <React.Fragment>
                 <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
+                    ESOP created successfully!
                 </Typography>
                 <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order confirmation, and will
-                  send you an update when your order has shipped.
+                  
                 </Typography>
               </React.Fragment>
             ) : (
